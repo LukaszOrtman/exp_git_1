0c0c40647053957344a383f2375341f128d3a99e
Lukasz Ortman <ortman.luk@gmail.com>
2023-11-09 18:55:54 +0100

    Deleted almost all.

--- page_1.py
+++ page_1.py
@@ -6,31 +6,6 @@ przez PLL LOT z warszawskiego lotniska im. Fryderyka Chopina, w gorącym
 „sylwestrowym” okresie. Ceny za połączenia rozpoczynają się od 274 PLN.
 Bez dodatkowych opłat polecisz z samym bagażem podręcznym i przedmiotem osobistym.
 
-Filip Rózga z Cracovii to jeden z czterech piłkarzy, którzy kilka dni temu uczestniczyli
-w skandalu reprezentacji Polski U-17. Jego fatalne zachowanie skomentował Jacek Zieliński.
-- Popełnił błąd, ale nie zamierzamy go skreślać. Teraz wszystko zależy od niego - m.in.
-powiedział trener Cracovii, cytowany przez Grzegorza Wojtowicza, dziennikarza PAP.
 
-Reprezentacja Polski U-17 przygotowuje się do mistrzostw świata, które odbędą się w Indonezji.
-Na kilka dni przed rozpoczęciem turnieju w kadrze wybuchł wielki skandal. Czterech piłkarzy
-zostało bowiem przyłapanych wieczorem na spożywaniu alkoholu. Wszyscy w nocy wymknęli się z hotelu
-i znaleziono ich w pobliskim barze. Byli to: Filip Rózga (Cracovia), Oskar Tomczyk, Filip Wolski
-(obaj Lech Poznań) oraz Jan Łabędzki (ŁKS Łódź). Decyzją PZPN wszyscy zostali odsunięci od drużyny
-narodowej.
-
-- Filip Rózga został przesunięty do zespołu juniorów, będzie też pracował jako wolontariusz z dziećmi
-w Szpitalu Uniwersyteckim. Popełnił błąd, ale nie zamierzamy go skreślać. Teraz wszystko zależy od niego
-- powiedział Jacek Zieliński. Jego wypowiedź zacytował Grzegorz Wojtowicz, dziennikarz Polskiej Agencji
-Prasowej na swoim profilu na Twitterze.
-
-Trzy dni temu Cracovia wydała oświadczenie na temat zachowania swojego młodego piłkarza.
-- Nasz Klub w pełni popiera decyzję sztabu reprezentacji Polski do lat 18, a także Polskiego Związku
-Piłki Nożnej. Zachowanie, za jakie zawodnicy zostali odsunięci z kadry, jest niegodne piłkarzy
-reprezentujących Polskę na dużym międzynarodowym turnieju, jakim są Mistrzostwa Świata do lat 17. Było
-to też zdarzenie, które mocno narusza zasady, jakimi kierować powinien się jakikolwiek piłkarz.
-    Profesjonalizmu i właściwego podejścia do sportu uczymy w naszej Akademii od najmłodszych lat i zdarzenie,
-którego uczestnikiem był także piłkarz Pasów, jest takim, które wymaga potępienia. Po powrocie Filipa
-Rózgi do Polski zapowiadamy wyciągnięcie surowych konsekwencji - czytamy na oficjalnej stronie klubowej
-Cracovii.
 
 
