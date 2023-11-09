37c40e2e60e824de1e5b5f3ab619dc4a088aff02
Lukasz Ortman <ortman.luk@gmail.com>
2023-11-09 17:42:58 +0100

    Added new info.

---
+++ page_1.py
@@ -0,0 +1,16 @@
+
+Jeśli jeszcze nie masz planów na ostatnią noc roku,
+skorzystaj z przygotowanego zestawienia biletów i przywitaj
+2024 za granicą. Tym razem będziesz miał okazję odwiedzić m.in.
+Węgry, Austrię, Niemcy, Włochy czy Słowację.
+
+Poniżej znajdziesz wybrane przeze mnie najciekawsze kierunki oferowane
+przez PLL LOT z warszawskiego lotniska im. Fryderyka Chopina, w gorącym
+„sylwestrowym” okresie. Ceny za połączenia rozpoczynają się od 274 PLN.
+Bez dodatkowych opłat polecisz z samym bagażem podręcznym i przedmiotem osobistym.
+
+Filip Rózga z Cracovii to jeden z czterech piłkarzy, którzy kilka dni temu uczestniczyli
+w skandalu reprezentacji Polski U-17. Jego fatalne zachowanie skomentował Jacek Zieliński.
+- Popełnił błąd, ale nie zamierzamy go skreślać. Teraz wszystko zależy od niego - m.in.
+powiedział trener Cracovii, cytowany przez Grzegorza Wojtowicza, dziennikarza PAP.
+
